fe90128204992286b9f59d2522f76ec39ed7d95e
Cường <dlvcuong.1711@gmail.com>
2026-09-24 13:36:35 +0700

    Phan 1: Khoi tao project Web API

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_simple.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from TestController");
+        }
+    }
+}
